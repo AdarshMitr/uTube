55347bd7e9443f74ea9e4d474def4cc798590f15
Abhimanu Sharma <dragonabv1@gmail.com>
2023-04-22 14:30:10 +0530

    fixed missing props

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,10 +4,11 @@ import {Sidebar,Videos} from './';
 
 
 const Feed = () => {
+  const setSelectedCategory = () => {}
   return (
     <Stack sx={{flexDirection:{sx:'column',md:'row'}}}>
       <Box sx={{height:{sx:'auto',md:'92vh'}, borderRight:'1px solid #3d3d3d', px:{sx:0,md:2}}}>
-<Sidebar/>
+<Sidebar selectedCategory={''} setSelectedCategory={setSelectedCategory}/>
 <Typography className='copyright'
 variant='body2'sx={{mt:1.5,
 color:'#fff'}}>
